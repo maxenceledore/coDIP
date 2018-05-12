f62d73292e373eb03abbfd54683c3771db7129d7
Maxence Le Doré <maxence.ledore@gmail.com>
2018-05-13 00:01:36 +0200

    move GLSL source to specific files.
    Will use script tag src attribute (with onload) later.

---
+++ edition/vs-image-geometry.glsl
@@ -0,0 +1,17 @@
+#version 300 es
+#define POSITION_LOCATION 0
+#define TEXCOORD_LOCATION 1
+
+precision highp float;
+precision highp int;
+
+layout(location = POSITION_LOCATION) in vec2 pos;
+layout(location = TEXCOORD_LOCATION) in vec2 texcoord;
+
+out vec2 tc;
+
+void main() {
+
+  gl_Position = vec4(pos, 0.0f, 1.0f);
+  tc = texcoord;
+}
