5e1625592965023a7beb27023e5d2efad4a84307
Maxence Le Doré <maxence.ledore@gmail.com>
2018-05-20 21:52:52 +0200

    Relicense under Affero GPL v3

--- vue.php
+++ vue.php
@@ -1,4 +1,24 @@
 <?php
+//    Copyright (C) 2018  Maxence Le Doré (maxence.ledore@gmail.com)
+
+//    This file is part of coDIP.
+
+//    coDIP is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of 
+//    the License, or (at your option) any later version.
+
+//    coDIP is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+
+//    You should have received a copy of the GNU Affero General Public
+//    License along with coDIP.
+//    If not, see <http://www.gnu.org/licenses/>.
+?>
+
+<?php
 
 if(isset($_GET['img_id']) && !empty($_GET['img_id'])) {
   $fichier = $_GET['img_id'];
