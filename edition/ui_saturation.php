f5be06d1d4c84034b7165607f0ac92853728e858
Maxence Le Doré <maxence.ledore@gmail.com>
2018-06-04 16:22:09 +0200

    ui-saturation: add field based saturation control.

--- edition/ui_saturation.php
+++ edition/ui_saturation.php
@@ -1,21 +1,34 @@
 <div class="edit-module" id="edit-mod-saturation" hidden=true>
   <p class="centrage"> <strong> SATURATION </strong> </p>
   <input type="range" min="-100" max="100" value="0" class="slider" id="satu"/>
+  <input type="number" min="-100" max="100" value="0" id="satu-value-field"/>
+  <script>
+    var curseur_satu = document.getElementById("satu");
+    var satu_value_field = document.getElementById("satu-value-field");
+    var saturation = satu_value_field.value;
+
+    curseur_satu.oninput = function() {
+      satu_value_field.value = curseur_satu.value;
+      saturation = curseur_satu.value;
+      set_saturation();
+      }
+    satu_value_field.oninput = function() {
+      curseur_satu.value = satu_value_field.value;
+      saturation = curseur_satu.value;
+      set_saturation();
+    }
+
+    function set_saturation() {
+      const COEFF_INTERNAL_MIN = 0.0;
+      const COEFF_INTERNAL_MAX = 2.0;
+      const UI_SATU_VAL_MIN = curseur_satu.min;
+      const UI_SATU_VAL_MAX = curseur_satu.max;
+      const m = (COEFF_INTERNAL_MAX-COEFF_INTERNAL_MIN)/(UI_SATU_VAL_MAX-UI_SATU_VAL_MIN);
+      const p = COEFF_INTERNAL_MAX - m*UI_SATU_VAL_MAX;
+      const coeff = m*curseur_satu.value+p;
+      gl.uniform1f(u_satu_coeff, coeff);
+      gl.uniform1ui(u_command, SATURATION_CONTROL);
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+      }
+  </script>
 </div>
-
-<script type="text/javascript">
-
-var curseur_satu = document.getElementById("satu");
-curseur_satu.oninput = function() {
-  const COEFF_INTERNAL_MIN = 0.0;
-  const COEFF_INTERNAL_MAX = 2.0;
-  const UI_SATU_VAL_MIN = curseur_satu.min;
-  const UI_SATU_VAL_MAX = curseur_satu.max;
-  const m = (COEFF_INTERNAL_MAX-COEFF_INTERNAL_MIN)/(UI_SATU_VAL_MAX-UI_SATU_VAL_MIN);
-  const p = COEFF_INTERNAL_MAX - m*UI_SATU_VAL_MAX;
-  const coeff = m*curseur_satu.value+p;
-  gl.uniform1f(u_satu_coeff, coeff);
-  gl.uniform1ui(u_command, SATURATION_CONTROL);
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-}
-</script>
